3dc78e51f2156bac6e6d6e6151ab1c420fa21b6f
Akira Hirose <ahirose@gmail.com>
2021-12-24 13:32:50 +0900

    added the remove animation

--- todo/app.js
+++ todo/app.js
@@ -1,3 +1,4 @@
+"use strict";
 //selectors
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
@@ -5,6 +6,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(event) {
@@ -22,6 +24,7 @@ function addTodo(event) {
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
   completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
+  //add trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
   trashButton.classList.add("trash-btn");
@@ -31,4 +34,24 @@ function addTodo(event) {
   todoList.appendChild(todoDiv);
   //clear todo input value
   todoInput.value = "";
+}
+
+function deleteCheck(event) {
+  const item = event.target;
+  //trash button
+
+  console.log(item.classList[0]);
+  if (item.classList[0] === "trash-btn") {
+    //find the todo element, then remove
+    const todo = item.parentElement;
+    //animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
