54492cdaff08e55cb1bb0a3887a929de51cbbe04
Akira Hirose <ahirose@gmail.com>
2021-12-31 22:20:06 +0900

    2021_12_31

--- Beatmaker/app.js
+++ Beatmaker/app.js
@@ -1,1 +1,23 @@
 // app.js;
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+  }
+  repeat() {
+    let step = this.index % 8;
+    console.log(step);
+    this.index++;
+  }
+
+  start() {
+    setInterval(() => {
+      this.repeat();
+    }, 1000);
+  }
+}
+const drumKit = new DrumKit();
+drumKit.start();
