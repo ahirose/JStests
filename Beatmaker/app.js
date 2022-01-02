8350cdaa58b82da67fdf02cb98645ea1314ebb93
Akira Hirose <ahirose@gmail.com>
2022-01-02 15:30:57 +0900

    2022_01_02

--- Beatmaker/app.js
+++ Beatmaker/app.js
@@ -16,7 +16,26 @@ class DrumKit {
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(step);
+    // console.log(step);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //check if pads are active
+      if (bar.classList.contains("active")) {
+        //check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
 
@@ -31,6 +50,9 @@ class DrumKit {
 const drumKit = new DrumKit();
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 drumKit.playBtn.addEventListener("click", () => {
   drumKit.start();
