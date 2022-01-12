5fac810b74ac390f5206fb8940da71f49341c531
Akira Hirose <ahirose@gmail.com>
2022-01-12 22:59:14 +0900

    2022_01_12

--- Beatmaker/app.js
+++ Beatmaker/app.js
@@ -3,14 +3,14 @@ class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
     this.playBtn = document.querySelector(".play");
-    this.currentKick = "./sounds/kick-classic.wav";
-    this.currentSnare = "./sounds/snare-acoustic01.wav";
-    this.currentHihat = "./sounds/hihat-acoustic01.wav";
+    // this.currentKick = "./sounds/kick-classic.wav";
+    // this.currentSnare = "./sounds/snare-acoustic01.wav";
+    // this.currentHihat = "./sounds/hihat-acoustic01.wav";
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
-    this.bpm = 150;
+    this.bpm = 300;
     this.isPlaying = null;
     this.selects = document.querySelectorAll("select");
     this.muteBtns = document.querySelectorAll(".mute");
