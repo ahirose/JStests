f0a91f672aa8b331320107e422af1d23de4a6b83
Akira Hirose <ahirose@gmail.com>
2022-01-14 22:40:50 +0900

    2022_01_14

--- Colors/app.js
+++ Colors/app.js
@@ -1,1 +1,26 @@
 // app.js;
+//global selections and variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll("input[type='range']");
+const currentHexes = document.querySelectorAll(".color h2");
+
+//functions
+
+//color generator
+function generateHex() {
+  const letters = "#0123456789ABCDEF";
+  let hash = "#";
+  for (let i = 0; i < 6; i++) {
+    hash += letters[Math.floor(Math.random() * 16)];
+  }
+  return hash;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    console.log(div.children[0]);
+  });
+}
+
+randomColors();
