9b0cbcaa6a5e79be74cb9fff6ca49eafdb5de950
Akira Hirose <ahirose@gmail.com>
2022-01-17 10:53:51 +0900

    2022_01_17

--- Colors/app.js
+++ Colors/app.js
@@ -28,6 +28,13 @@ function randomColors() {
     hexText.innerText = randomColor;
     //check for contrast
     checkTextContrast(randomColor, hexText);
+    //   Initial colorize sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -38,6 +45,34 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //scale saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+  //Scale brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  //Update input colors
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )},${scaleSat(1)})`;
+
+  //Update input brightness
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0
+  )},${scaleBright(0.5)},${scaleBright(1)})`;
+
+  //update input hue
+  hue.style.backgroundImage = `linear-gradient(to right,
+    rgb(204,75,75),rgb(204,204,75),
+    rgb(75,204,75),rgb(75,204,204),
+    rgb(75,75,204),rgb(204,75,204),
+    rgb(204,75,75))`;
 }
 
 randomColors();
